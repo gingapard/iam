124c5fa67265767cc710f65b4e699c4d292e6f76
gapard <cbroo@duck.com>
2024-07-01 22:32:50 +0200

    remove moveCell

--- src/map.c
+++ src/map.c
@@ -10,40 +10,6 @@ void initMap(Map* map) {
     }
 }
 
-void moveCell(Cell grid[GRID_WIDTH][GRID_HEIGHT], int x, int y, Direction direc) {
-
-    // is in  bounds
-    if (x < 0 || x >= GRID_WIDTH || y < 0 || y >= GRID_HEIGHT) {
-        return;
-    }
-
-    int new_x = x;
-    int new_y= y;
-    switch (direc) {
-        case Up:
-            new_y = y - 1;
-            break;
-        case Down:
-            new_y = y + 1;
-            break;
-        case Left:
-            new_x = x - 1;
-            break;
-        case Right:
-            new_x = x + 1;
-            break;
-        default:
-            return;
-    }
-
-    if (new_x < 0 || new_x >= GRID_WIDTH || new_y < 0 || new_y >= GRID_HEIGHT) {
-        return;
-    }
-
-    grid[new_y][new_x] = grid[y][x];
-    grid[y][x].type = Air; 
-}
-
 void drawMap(SDL_Renderer* rend, Map* map) {
     for (int y = 0; y < GRID_HEIGHT; ++y) { 
         for (int x = 0; x < GRID_WIDTH; ++x) { 
