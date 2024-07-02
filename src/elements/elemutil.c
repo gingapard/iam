efde9b172de1e83aeef1e2bfaea867a2a323f401
gingapard <trenchwhisper@protonmail.com>
2024-07-02 22:27:00 +0200

    ElementTrait

---
+++ src/elements/elemutil.c
@@ -0,0 +1,5 @@
+#include "../constants.h"
+#include "../map.h"
+
+void fallDown(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
+}
---
+++ src/elements/elemutil.h
@@ -0,0 +1,9 @@
+#ifndef ELEMUTIL_H
+#define ELEMUTIL_H
+
+#include "../constants.h"
+#include "../map.h"
+
+void fallDown(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x);
+
+#endif
