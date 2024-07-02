5c6e8497b279a3227f3f322d85bd6ced2b3f8270
matfk <mfred@tuta.com>
2024-07-02 19:55:25 +0200

    hold down buttons

--- src/globals.h
+++ src/globals.h
@@ -7,6 +7,8 @@
 extern Uint8 should_close;
 extern Uint8 element_type;
 extern float current_fps;
+extern Uint8 mouse_left_held;
+extern Uint8 mouse_right_held;
 
 extern TTF_Font* font;
 extern SDL_Renderer* rend;
