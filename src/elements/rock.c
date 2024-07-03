95e40fe2fe856727d77892c086da9efd8540ac3e
matfk <mfred@tuta.com>
2024-07-03 14:51:35 +0200

    fixed rock phsysics

--- src/elements/rock.c
+++ src/elements/rock.c
@@ -1,7 +1,7 @@
 #include "../constants.h"
 #include "../map.h"
 
-void updateRock(Cell grid[GRID_HEIGHT][GRID_WIDTH], int x, int y) {
+void updateRock(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
     if (y + 1 < GRID_HEIGHT && grid[y + 1][x].type == Air) {
         grid[y + 1][x] = grid[y][x];
         grid[y][x].type = Air;
--- src/elements/sand.c
+++ src/elements/sand.c
@@ -3,10 +3,13 @@
 #include "../map.h"
 
 void updateSand(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
+    // Checking if sand can move one down
     if (y + 1 < GRID_HEIGHT && grid[y + 1][x].type == Air) {
         grid[y + 1][x] = grid[y][x];
         grid[y][x].type = Air;
     } else if (grid[y][x].type == Sand) {
+
+        // If both left and right is free, it will pick a random direction
         if (x > 0 && grid[y][x - 1].type == Air && grid[y + 1][x - 1].type == Air && x < GRID_WIDTH - 1 && grid[y][x + 1].type == Air && grid[y + 1][x + 1].type == Air) {
             if (rand() % 2) {
                 grid[y + 1][x - 1] = grid[y][x];
@@ -17,7 +20,8 @@ void updateSand(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
                 grid[y][x].type = Air;
             }
         }
-        // Check if sand can accumilate
+
+        // Else if just one direction is free, it will take that
         else if (x > 0 && grid[y][x - 1].type == Air && grid[y + 1][x - 1].type == Air) {
             grid[y + 1][x - 1] = grid[y][x];
             grid[y][x].type = Air;
