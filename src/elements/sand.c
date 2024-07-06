6ab5a3fe12a2719ab7e1de3ee54359f551d1e466
gingapard <trenchwhisper@protonmail.com>
2024-07-06 02:46:10 +0200

    rewrite sand.c

--- src/elements/sand.c
+++ src/elements/sand.c
@@ -2,36 +2,29 @@
 #include "../constants.h"
 #include "../map.h"
 
-void updateSand(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
+void moveRandom(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
+    if (rand() % 2)
+        downRightSwap(grid, y, x);
+    else 
+        downLeftSwap(grid, y, x);
+}
 
-    // Checking if sand can move one down
+void updateSand(Cell grid[GRID_HEIGHT][GRID_WIDTH], int y, int x) {
+    // moving directly down
     if (y + 1 < GRID_HEIGHT && hasTrait(grid[y + 1][x].type, Empty)) {
         downSwap(grid, y, x);
-    } else {
+        return;
+    }
+    
+    Uint8 canLeft = (x > 0 && hasTrait(grid[y][x - 1].type, Empty) && hasTrait(grid[y + 1][x - 1].type, Empty));
+    Uint8 canRight = (x < GRID_WIDTH - 1 && hasTrait(grid[y][x + 1].type, Empty) && hasTrait(grid[y + 1][x + 1].type, Empty));
 
-        // If both left and right is free, it will pick a random direction
-        if (x > 0
-            && hasTrait(grid[y][x - 1].type, Empty) 
-            && hasTrait(grid[y + 1][x - 1].type, Empty)
-            && x < GRID_WIDTH - 1
-            && hasTrait(grid[y][x + 1].type, Empty)
-            && hasTrait(grid[y + 1][x + 1].type, Empty)
-        ) {
-            if (rand() % 2) {
-                downRightSwap(grid, y, x);
-            }
-            else {
-                downLeftSwap(grid, y, x);
-            }
-        }
-
-        // Else if just one direction is free, it will take that
-        else if (x > 0 && hasTrait(grid[y][x - 1].type, Empty) && hasTrait(grid[y + 1][x - 1].type, Empty)) {
-            grid[y + 1][x - 1] = grid[y][x];
-            grid[y][x].type = Air;
-        } else if (x < GRID_WIDTH - 1 && hasTrait(grid[y][x + 1].type, Empty) && hasTrait(grid[y + 1][x + 1].type, Empty)) {
-            grid[y + 1][x + 1] = grid[y][x];
-            grid[y][x].type = Air;
-        }
+    if (canLeft && canRight) {
+        moveRandom(grid, y, x);
+    } else if (canRight) {
+        downRightSwap(grid, y, x);
+    } else if (canLeft) {
+        downLeftSwap(grid, y, x);
     }
 }
+
